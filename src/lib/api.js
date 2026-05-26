4de52b54e3d59167b72809f7c327fc7c38093e8c
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2026-05-26 22:59:11 +0800

    feat: configure api base url from env

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || 'http://127.0.0.1:8000/api';
+
 export const api = axios.create({
-  // baseURL: 'https://be.hmjti-uinam.or.id/api',
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: API_BASE_URL,
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
